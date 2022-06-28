bc7785c69be0aae794b4bd9a3a8cde22af366d5f
henriquedafonte <henriquegdafonte@gmail.com>
2022-06-27 22:53:45 -0400

    initial Deploy

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,80 @@
+import { CheckCircle, Lock } from 'phosphor-react'
+//isPast Verifica se uma data ja passou
+import { isPast, format } from 'date-fns'
+import { Link, useParams } from 'react-router-dom'
+import classNames from 'classnames'
+
+interface LessonProps {
+  title: string
+  slug: string
+  availableAt: Date
+  type: 'live' | 'class'
+}
+
+export function Lesson(props: LessonProps) {
+  const { slug } = useParams<{ slug: string }>()
+
+  const isLessonAvailable = isPast(props.availableAt)
+  //as siglas usadas para formatar a data foi buscada do site date-fns.org/docs
+  const availableDateFormatted = format(
+    props.availableAt,
+    "EEEE' • 'MMMM d' • 'k'h'mm"
+  )
+
+  const isActiveLesson = slug === props.slug
+
+  return (
+    <Link to={`/event/lesson/${props.slug}`} className="group">
+      <span className="text-gray-300">{availableDateFormatted}</span>
+      <div
+        className={classNames(
+          'rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500',
+          {
+            'bg-green-500': isActiveLesson
+          }
+        )}
+      >
+        <header className="flex items-center justify-between">
+          {isLessonAvailable ? (
+            <span
+              className={classNames(
+                'flex items-center gap-2 text-sm  font-medium',
+                {
+                  'text-white': isActiveLesson,
+                  'text-blue-500': !isActiveLesson
+                }
+              )}
+            >
+              <CheckCircle size={20} />
+              Content released
+            </span>
+          ) : (
+            <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+              <Lock size={20} />
+              Coming soon
+            </span>
+          )}
+          <span
+            className={classNames(
+              'text-xs rounded py-[0.125rem] px-2 text-white border font-bold',
+              {
+                'border-white': isActiveLesson,
+                'border-green-300': !isActiveLesson
+              }
+            )}
+          >
+            {props.type === 'live' ? 'Live' : 'Video lessons'}
+          </span>
+        </header>
+        <strong
+          className={classNames('mt-5 block', {
+            'text-white': isActiveLesson,
+            'text-gray-200': !isActiveLesson
+          })}
+        >
+          {props.title}
+        </strong>
+      </div>
+    </Link>
+  )
+}
